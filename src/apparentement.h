2d7b5c566433c0371f6401ce1ba3d42d5f0c18d5
mohan-rr <mohan.r.rakesh@gmail.com>
2022-11-30 16:56:49 -0500

    v1.1.7
    CRAN web checks:
    remove sprintf, remove unrecognized UTF characters in comments

--- src/apparentement.h
+++ src/apparentement.h
@@ -2,8 +2,8 @@
 \brief Interface des fonctions de calcul de Phi
 
 
-\author S�bastien Leclerc
-\contributor Jean-Fran�ois Lefebvre
+\author Sbastien Leclerc
+\contributor Jean-Franois Lefebvre
 
 */
 
@@ -14,7 +14,7 @@
 #include "hashtable.h"
 #include <Rcpp.h>
 
-/// Structure de gestion memoire m�moire utilise par kinship2
+/// Structure de gestion memoire mmoire utilise par kinship2
 /**
 	Utiliser pour effectuer une serie d'assignement memoire et ensuite laisser la voie libre au
 	gestionnaire de memoire pour qu'il effectue lui-meme la "gargage collection"
@@ -30,7 +30,7 @@ struct Kinship4Struct
 private:	
 	short NiveauMax; //Niveau maximal voulu 0..Niveaumax = NiveauMax+1 Case...
 	BlockAlloc<double> memblock; 
-	SMPile<double*,MAX_SUPPORTED_GENERATION> PileCosan; //L'adresse de la cosanguinit� a �crire
+	SMPile<double*,MAX_SUPPORTED_GENERATION> PileCosan; //L'adresse de la cosanguinit a crire
 
 	//SUPPORT MT		
 	static CSema m_acces; //Semaphore pour l'acces a allele qui sert de variable partage	
@@ -39,18 +39,24 @@ public:
 	Kinship4Struct();	
 	Kinship4Struct(short NiveauMax, double* resultat); //Resultat tableau de taille NiveauMax+1		
 	void Initialise(short NiveauMax, double* Resultat);
-	static void InitMT();
-	static void ReleaseMT();
-	inline void remplace(double* resultat) {PileCosan.pop();PileCosan.push(resultat);} 
+
+	#ifndef __APPLE__
+		static void InitMT();
+		static void ReleaseMT();
+		inline void remplace(double* resultat) {PileCosan.pop();PileCosan.push(resultat);} 
+		
+	#endif
 	
-    //FONCTION AMIS
+	//FONCTION AMIS
 	friend void FASTCALL Kinship4(CIndSimul* Ind1, CIndSimul* Ind2, short ttl1, short ttl2,Kinship4Struct &T);
+	
+	#ifndef __APPLE__
 	friend void FASTCALL Kinship4MT(CIndSimul* Ind1, CIndSimul* Ind2, short ttl1, short ttl2,Kinship4Struct &T);	
+	#endif
 };
 
 int Phis(int* Genealogie, int* proposant, int NProposant,int NiveauMin,int NiveauMax, double* pdMoyenne,double *MatrixArray, int printprogress);
 int PhisMT(int* Genealogie, int* proposant, int NProposant,int NiveauMin,int NiveauMax, double* pdMoyenne,double *MatrixArray, int printprogress);
-
 
 double FASTCALL Kinship(CIndSimul* Ind1,CIndSimul* Ind2,short ttl1,short ttl2);
 void FASTCALL Kinship4(CIndSimul* Ind1, CIndSimul* Ind2, short ttl1, short ttl2,Kinship4Struct &T);
